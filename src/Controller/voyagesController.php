8995d4ccfe06e208578ccc5b237986cc43d39127
Pandouci <irahaoui@yahoo.fr>
2022-09-22 12:51:15 +0200

    phase 11 : ORM (nouvelle page pour l'affichage détaillé d'un enregistrement.

--- src/Controller/voyagesController.php
+++ src/Controller/voyagesController.php
@@ -63,5 +63,15 @@ class voyagesController extends AbstractController {
         $visites=$this->repository->findByEqualValue($champ, $valeur);
         return $this->render("pages/voyages.html.twig",['visites'=>$visites]);
     }
+    /**
+     * @Route("/voyages/voyage/{id}", name="voyage.showone")
+     * @param type $id
+     * @return Response
+     */
+    public function showOne($id) : Response{
+        $visite =$this->repository->find($id);
+        return $this->render("pages/voyage.html.twig",['visite'=>$visite]);
+    }
+    
     
 }
