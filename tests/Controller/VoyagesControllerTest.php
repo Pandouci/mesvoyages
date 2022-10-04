c5be999cb3b67264e8a61d65b0a344bad8498561
Pandouci <irahaoui@yahoo.fr>
2022-10-04 21:35:02 +0200

    phase 23 : tests.

---
+++ tests/Controller/VoyagesControllerTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Description of VoyagesControllerTest
+ *
+ * @author emds
+ */
+class VoyagesControllerTest extends WebTestCase {
+    
+    const CHEMIN_VOYAGES = '/voyages';
+    
+    public function testAccesPage(){
+        $client = static::createClient();
+        $client->request('GET', self::CHEMIN_VOYAGES);
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+    }
+    
+    public function testContenuPage(){
+        $client = static::createClient();
+        $crawler = $client->request('GET', self::CHEMIN_VOYAGES);
+        $this->assertSelectorTextContains('h1', 'Mes voyages');
+        $this->assertSelectorTextContains('th', 'Ville');
+        $this->assertCount(4, $crawler->filter('th'));
+        $this->assertSelectorTextContains('h5', 'Margon');
+    }
+    
+    public function testLinkVille(){
+        $client = static::createClient();
+        $client->request('GET', self::CHEMIN_VOYAGES);
+        // clic sur le lien (le nom d'une ville)
+        $client->clickLink('Margon');
+        // récupération du résultat du clic
+        $response = $client->getResponse();
+        dd($client->getRequest());
+//        dd($client->getRequest());
+        // contrôle si le client existe
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        // récupération de la route et contrôle qu'elle est correcte
+        $uri = $client->getRequest()->server->get('REQUEST_URI');
+        $this->assertEquals('/voyages/voyage/101', $uri);
+    }
+    
+    public function testFiltreVill(){
+        $client = static::createClient();
+        $client->request('GET', self::CHEMIN_VOYAGES);
+        // simulation de la soumission du formaulaire
+        $crawler = $client->submitForm('filtrer', [
+            'recherche' => 'Paris'
+        ]);
+        // vérifie le nombre de lignes obtenues
+        $this->assertCount(1, $crawler->filter('h5'));
+        // vérifie si la ville correspond à la recherche
+        $this->assertSelectorTextContains('h5', 'Paris');
+    }
+    
+}
+
---
+++ tests/Repository/VisiteRepositoryTest.php
@@ -0,0 +1,70 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Test\Repository;
+
+use App\Entity\Visite;
+use App\Repository\VisiteRepository;
+use DateTime;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+/**
+ * Description of VisiteRepositoryTest
+ *
+ * @author Station
+ */
+class VisiteRepositoryTest extends KernelTestCase {
+    
+    public function recupRepository(): VisiteRepository {
+        self::bootKernel();
+        $repository= self::getContainer()->get(VisiteRepository::class);
+        return $repository;
+    }
+    
+    public function testNbVisite() {
+        $repository = $this->recupRepository();
+        $nbVisites= $repository->count([]);
+        $this->assertEquals(100,$nbVisites);
+    }
+    
+    public function newVisite(): Visite{
+        $visite= (new Visite())
+                ->setVille("New York")
+                ->setPays('USA')
+                ->setDatecreation(new DateTime("now"));
+        return $visite;
+    }
+    
+    public function testAddVisite() {
+        $repository= $this->recupRepository();
+        $visite= $this->newVisite();
+        $nbVisites=$repository->count([]);
+        $repository->add($visite,true);
+        $this->assertEquals($nbVisites+1, $repository->count([]),"erreur lors de l'ajout");
+        
+    }
+    
+    public function testRemoveVisite() {
+        $repository= $this->recupRepository();
+        $visite = $this->newVisite();
+        $repository->add($visite,true);
+        $nbVisites= $repository->count([]);
+        $repository->remove($visite,true);
+        $this->assertEquals($nbVisites-1,$repository->count([]),"erreur lors de la suppression");
+    }
+    
+    public function testFindByEqualValue() {
+        $repository= $this->recupRepository();
+        $visite = $this->newVisite();
+        $repository->add($visite,true);
+        $visites=$repository->findByEqualValue("ville", "New York");
+        $nbVisites = count($visites);
+        $this->assertEquals(1,$nbVisites);
+        $this->assertEquals("New York", $visites[0]->getVille());
+    }
+}
